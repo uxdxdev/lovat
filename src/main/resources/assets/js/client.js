d00cfbd776ed839b889ec14d0ba5eaed7d273404
David Morton <davidmortonmail@gmail.com>
2017-05-08 14:35:42 +0100

    Removed client.js, updated bundle.js, and updated index template

--- src/main/resources/assets/js/client.js
+++
@@ -1,8 +0,0 @@
-var loadEventsFromServer = function(eventsUrl) {
-	axios.get(eventsUrl).then(function(res) {
-		var data = res.data.list;		
-		renderClientEvents(data);
-	});
-};
-
-loadEventsFromServer('https://dropwizardheroku-webgateway.herokuapp.com/events');
