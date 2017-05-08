c29a2b4a57cef9ba1aede9b052fb1e3c4bd74ca2
David Morton <davidmortonmail@gmail.com>
2017-05-08 09:41:52 +0100

    Updated webpack build config to output bundle.js to the src AND build directories for Heroku to include bundle.js in the jar file

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,47 @@ var webpack = require('webpack')
 var ROOT = path.resolve(__dirname, 'src/main/resources/assets');
 var SRC = path.resolve(ROOT, 'jsx');
 var DEST = path.resolve(ROOT, 'js');
+var BUILD_ROOT = path.resolve(__dirname, 'build/resources/main/assets');
+var BUILD_DEST = path.resolve(BUILD_ROOT, 'js');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    app: SRC + '/index.jsx',
-  },
-  resolve: {
-    modules: [
-      path.resolve(ROOT, 'jsx')
-    ],
-    extensions: ['*', '.js', '.jsx']    
-  },
-  output: {
-    path: DEST,
-    filename: 'bundle.js',
-    publicPath: '/js/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loaders: 'babel-loader',
-        include: SRC,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
+	devtool : 'source-map',	
+	resolve : {
+		modules : [ path.resolve(ROOT, 'jsx') ],
+		extensions : [ '*', '.js', '.jsx' ]
+	},
+	module : {
+		loaders : [ {
+			test : /\.js?$/,
+			exclude : /node_modules/,
+			loaders : 'babel-loader',
+			include : SRC,
+			query : {
+				presets : [ 'es2015', 'react' ]
+			}
+		} ]
+	}
 }
+
+module.exports = [
+                  {
+                	  entry : {
+                			app : SRC + '/index.jsx',
+                		},
+                		output : {
+                			path : DEST,
+                			filename : 'bundle.js',
+                			publicPath : '/js/'
+                		},
+                  },
+                  {
+                	  entry : {
+                			app : SRC + '/index.jsx',
+                		},
+                		output : {
+                			path : BUILD_DEST,
+                			filename : 'bundle.js',
+                			publicPath : '/js/'
+                		},
+                  }
+                  ];
