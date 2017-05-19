845de9b1909bee624ae3fb89a8bacd61428d9f32
David Morton <davidmortonmail@gmail.com>
2017-05-19 12:24:23 +0100

    Refactored reactjs components

--- src/main/resources/webapp/js/EventsList.js
+++ src/main/resources/webapp/js/EventsList.js
@@ -27,18 +27,21 @@ var Event = React.createClass({
 	},
 	render(){
 		return (
-			React.createElement('li', {className: 'EventsList-item', key : this.state.index},
-				React.createElement('h2', {className: 'EventsListItem-name'}, this.state.data.name),
-				React.createElement('div', null, this.state.data.description),
-				React.createElement('div', null, this.state.data.location),
-				React.createElement('div', null, this.state.data.date),
+			<li className='EventsList-item' key={this.state.index}>
+				<h2 className='EventsListItem-name'>{this.state.data.name}</h2>
+				<div>{this.state.data.description}</div>
+				<div>{this.state.data.location}</div>
+				<div>{this.state.data.date}</div>
 				<Buttons/>
-			)
+			</li>
 		)
 	}
 })
 
 var EventsList = React.createClass({
+	propTypes: {
+    events: React.PropTypes.array.isRequired
+  },
 	getInitialState() {
 		return {
 			events : this.props.events,
@@ -61,14 +64,16 @@ var EventsList = React.createClass({
 		setInterval(this.loadEventsFromServer.bind(null, this, this.state.url), this.state.pollInterval);
 	},
 	render(){
+		const events = this.state.events.map(function(event, index) {
+			return <Event data={event} index={index}/>
+		});
 		return (
-			React.createElement('div', {className: 'EventsList'},
-					React.createElement('h2', null, 'Events List'),
-					React.createElement('ul', {className: 'EventsList-list'}, this.state.events.map(function(event, index) {
-						return <Event data={event} index={index}/>
-					})
-					)
-			)
+			<div className='EventsList'>
+				<h2>Events List</h2>
+				<ul className='EventsList-list'>
+					{events}
+				</ul>
+			</div>
 		)
 	}
 })
