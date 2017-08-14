213f14bf3cf3f6eb0764c39a7bf933d3cf522605
David Morton <davidmortonmail@gmail.com>
2017-08-14 10:56:23 +0100

    Added warning supressions to unit test

--- src/test/java/com/bitbosh/lovat/webgateway/api/ApiResponseUnitTest.java
+++ src/test/java/com/bitbosh/lovat/webgateway/api/ApiResponseUnitTest.java
@@ -7,28 +7,27 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.bitbosh.lovat.webgateway.api.ApiResponse;
-
+@SuppressWarnings("unchecked")
 public class ApiResponseUnitTest {
 
 	@Test
-	  public void getEvents_returnsCorrectList_IfInjectedListNotNull() {
-	    List<String> expectedList = new ArrayList<String>();
-	    String expectedEntry = "test";
-	    expectedList.add(expectedEntry);
-	    ApiResponse response = new ApiResponse(expectedList);
-	    List<String> actualList = (List<String>) response.getList();
-	    String actualEntry = actualList.get(0);
+	public void getEvents_returnsCorrectList_IfInjectedListNotNull() {
+		List<String> expectedList = new ArrayList<String>();
+		String expectedEntry = "test";
+		expectedList.add(expectedEntry);
+		ApiResponse response = new ApiResponse(expectedList);
+		List<String> actualList = (List<String>) response.getList();
+		String actualEntry = actualList.get(0);
 
-	    assertEquals(expectedList.size(), actualList.size());
-	    assertEquals(expectedEntry, actualEntry);	    
-	  }
+		assertEquals(expectedList.size(), actualList.size());
+		assertEquals(expectedEntry, actualEntry);
+	}
 
-	  @Test
-	  public void getEvents_returnsNull_IfInjectedListNull() {
-	    List<String> expectedList = null;
-	    ApiResponse response = new ApiResponse(expectedList);
-	    List<String> actualList = (List<String>) response.getList();
-	    assertEquals(expectedList, actualList);
-	  }
+	@Test
+	public void getEvents_returnsNull_IfInjectedListNull() {
+		List<String> expectedList = null;
+		ApiResponse response = new ApiResponse(expectedList);
+		List<String> actualList = (List<String>) response.getList();
+		assertEquals(expectedList, actualList);
+	}
 }
