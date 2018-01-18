4b67e220cfe047833dbafe19f97f5bf8e3b6c94c
David Morton <davidmortonmail@gmail.com>
2018-01-18 10:49:01 +0000

    Added logo to header

--- frontend/webapp/js/Header.js
+++ frontend/webapp/js/Header.js
@@ -5,8 +5,8 @@ class Header extends React.Component {
 		return (
 		    <header className="navbar flex-md-row">
                 <div className="nav">
-                    <a className="navbar-brand" href="/">Lovat</a>
-                    <a className="nav-link" href="https://travis-ci.org/damorton/lovat" target="_blank" ><img src="https://api.travis-ci.org/damorton/lovat.svg?branch=master" alt="Travis build status" /></a>
+                    <a className="navbar-brand" href="/"><img className="d-inline-block align-top mr-2" src="assets/favicon.png" alt="logo" />Lovat</a>
+                    <a className="nav-link" href="https://travis-ci.org/damorton/lovat" target="_blank" ><img className="d-inline-block align-top" src="https://api.travis-ci.org/damorton/lovat.svg?branch=master" alt="Travis build status" /></a>
                 </div>
                 <div className="nav ml-md-auto">
                     <a className="nav-link" href="/login">Login</a>
